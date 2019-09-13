5c8eec9100e371646d66bc41c5f572707d56e231
Michael Dudek <michael.dudek@centrum.cz>
2019-09-13 15:32:34 +0200

    - added support for dynamicLinkDomain property in ActionCodeSettings when sendSignInWithEmailLink is called - Android only

--- packages/firebase_auth/android/src/main/java/io/flutter/plugins/firebaseauth/FirebaseAuthPlugin.java
+++ packages/firebase_auth/android/src/main/java/io/flutter/plugins/firebaseauth/FirebaseAuthPlugin.java
@@ -336,6 +336,7 @@ public class FirebaseAuthPlugin implements MethodCallHandler {
   private void handleSendLinkToEmail(MethodCall call, Result result, FirebaseAuth firebaseAuth) {
     Map<String, Object> arguments = call.arguments();
     String email = arguments.get("email").toString();
+    Object dynamicLinkDomain = arguments.containsKey("dynamicLinkDomain") ? arguments.get("dynamicLinkDomain") : null;
     ActionCodeSettings actionCodeSettings =
         ActionCodeSettings.newBuilder()
             .setUrl(arguments.get("url").toString())
@@ -345,6 +346,7 @@ public class FirebaseAuthPlugin implements MethodCallHandler {
                 arguments.get("androidPackageName").toString(),
                 (Boolean) arguments.get("androidInstallIfNotAvailable"),
                 arguments.get("androidMinimumVersion").toString())
+            .setDynamicLinkDomain(dynamicLinkDomain != null ? dynamicLinkDomain.toString() : null)
             .build();
 
     firebaseAuth
